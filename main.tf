70c4aeb9f7e88fb48efdf670aeaf4d48bbd1a0a1
Saravanan.B <saravanannus@gmail.com>
2024-09-28 11:38:23 +0800

    after add yaml update

--- main.tf
+++ main.tf
@@ -176,3 +176,61 @@ resource "aws_lambda_permission" "api_gw" {
 
   source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
+# # Lookup Route 53 zone by domain name
+# data "aws_route53_zone" "zone" {
+#   name = "sctp-sandbox.com"
+# }
+
+# # ACM module to create an SSL certificate for the custom domain
+# module "acm" {
+#   source  = "terraform-aws-modules/acm/aws"
+#   version = "~> 4.0"
+
+#   domain_name       = "${local.name_prefix}.sctp-sandbox.com"
+#   zone_id           = data.aws_route53_zone.zone.zone_id
+#   validation_method = "DNS"
+# }
+
+# # Define the custom domain name for the API Gateway
+# resource "aws_apigatewayv2_domain_name" "http-api" {
+#   domain_name = "${local.name_prefix}.sctp-sandbox.com"
+
+#   domain_name_configuration {
+#     certificate_arn = module.acm.acm_certificate_arn
+#     endpoint_type   = "REGIONAL"  # Change this to "EDGE" if needed
+#     security_policy = "TLS_1_2"
+#   }
+# }
+
+# # Define the API Gateway (HTTP API)
+# resource "aws_apigatewayv2_api" "my_api" {
+#   name          = "my-http-api"
+#   protocol_type = "HTTP"
+# }
+
+# # Define the stage for the API Gateway
+# resource "aws_apigatewayv2_stage" "example" {
+#   api_id      = aws_apigatewayv2_api.my_api.id
+#   name        = "prod"
+#   auto_deploy = true
+# }
+
+# # Map the API to the custom domain
+# resource "aws_apigatewayv2_api_mapping" "example" {
+#   api_id      = aws_apigatewayv2_api.my_api.id
+#   domain_name = aws_apigatewayv2_domain_name.http-api.domain_name
+#   stage       = aws_apigatewayv2_stage.example.name
+# }
+
+# # Route53 record to map the custom domain to the API Gateway domain name
+# resource "aws_route53_record" "http-api" {
+#   zone_id = data.aws_route53_zone.zone.zone_id
+#   name    = "${local.name_prefix}.sctp-sandbox.com"
+#   type    = "A"
+
+#   alias {
+#     name                   = aws_apigatewayv2_domain_name.http-api.domain_name_configuration[0].target_domain_name
+#     zone_id                = aws_apigatewayv2_domain_name.http-api.domain_name_configuration[0].hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
